5586e82b1ba19c7f630a73b2ab2fb04059f6a893
fbjorn <denis@fbjorn.cc>
2023-01-20 16:49:38 +0300

    Fix deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit/vite'
+// const dev = process.argv.includes('dev')
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,6 +10,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			// base: dev ? '' : '/homepage',
+		},
+		appDir: 'app_',
 		adapter: adapter()
 	}
 };
